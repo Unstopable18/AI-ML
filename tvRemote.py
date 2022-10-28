e4b84fe0a78cc34f3b9858f181db81d53adb5f04
Vaishnavi Ravindra Deshmukh <vaishnavirdeshmukh18@gmail.com>
2022-10-28 17:24:25 +0530

    remote

--- tvRemote.py
+++ tvRemote.py
@@ -1,45 +1,60 @@
-from operator import truediv
 from time import sleep
+
 class remote():
     def __init__(self):
         self.power=False
         self.volume=0
-        self.mute=False
+        self.mute=True
 
     def onP(self):
-        sleep()
         self.power=True
+        print('🟢-Turning TV ON')
+        my_str = 'SAMSUNG'
+        for char in my_str:
+            print(char, end=' ', flush=True)
+            sleep(0.5)
+        print('')
     
     def offP(self):
         self.power=False
+        print('🔴-Turning TV OFF')
     
     def onM(self):
         if(self.power==False):
             print('Please turn on TV')
         else:
             self.mute=True
+            print('🔇')
 
     def offM(self):
         if(self.power==False):
             print('Please turn on TV')
         else:
             self.mute=False
+            print('🔊')
 
     def inV(self):
         if(self.power==False):
             print('Please turn on TV')
         else:
-            if(self.mute==False):
-                self.offM()
+            if(self.mute==True):
+                self.mute=False
             if(self.volume<10):
                 self.volume+=1
+                print(self.volume,'-🔊')
 
     def deV(self):
         if(self.power==False):
             print('Please turn on TV')
         else:
-            if(self.volume>0):
+            if(self.volume>=1):
                 self.volume-=1
+                if(self.volume==0):
+                    self.onM()
+                else:
+                    print(self.volume,'-🔉')
+            
+            
 
     def show(self):
         if(self.power==False):
@@ -47,13 +62,18 @@ class remote():
         else:
             print("Power:\t",self.power,'\nVolume:\t',self.volume,'\nMute:\t',self.mute)
 r=remote()
-r.show()
-r.inV()
 r.onP()
+r.show()
 r.inV()
 r.inV()
 r.show()
 r.deV()
 r.show()
+r.deV()
+r.deV()
+r.deV()
+r.show()
+
+
 
 
